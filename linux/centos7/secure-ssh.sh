1aba3787284397995f4c95ab4320b3f68641f464
Lillian-Griffiths <lillian.griffiths@mymail.champlain.edu>
2026-02-17 17:37:57 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -24,4 +24,4 @@ chmod 600 /home/$USERNAME/.ssh/authorized_keys
 chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
 
 sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-systemctl restart ssh
+systemctl restart sshd
